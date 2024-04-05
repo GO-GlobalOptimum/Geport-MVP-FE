13865c022356cd94bc14d22cae42c0c252bc4e88
youhyunwoo99 <casshyunwoo@naver.com>
2024-04-05 11:59:24 +0900

    [ADD] view 페이지 추가

---
+++ src/app/hidden-code/page.js
@@ -0,0 +1,70 @@
+"use client"
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function HiddenCodePage() {
+    const router = useRouter();
+    const [hiddenCode, setHiddenCode] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // 여기에 히든코드 검증 및 처리 로직을 추가할 수 있습니다.
+        // 예를 들어, 서버에 히든코드를 전송하고 유효한 코드인지 확인하는 등의 로직을 수행할 수 있습니다.
+        // 이 예시에서는 히든코드가 유효하다고 가정하고 "/" 경로로 이동합니다.
+
+        // 입력값이 '1'이면 해당 값을 id로 설정하여 view/[id] 페이지로 이동
+        if (hiddenCode.trim() === '1') {
+            router.push(`/view/${hiddenCode}`);
+        } else {
+            // 다른 경우에는 기본 경로인 '/'로 이동
+            router.push('/');
+        }
+    };
+
+    const isButtonDisabled = hiddenCode.trim() === ''; // 입력값이 비어있으면 버튼 비활성화
+
+    return (
+        <div style={{backgroundColor: "#181818", height: '100vh', color: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{marginBottom: '40px'}}>
+                <div style={{marginTop:'100px'}}>
+                    <h2 style={{color:'white', fontSize: '24px'}}>히든코드를 입력해주세요</h2>
+                </div>
+                <br/>
+                <div>
+                    <span style={{color: "#A8A8A8"}}>모든 문항에 응답하셨다면 Geport를 열람할 수 있는</span>
+                    <br />
+                    <span style={{color: "#A8A8A8"}}>히든코드를 메시지로 보내드립니다.</span>
+                </div>
+            </div>
+            <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <input
+                    type="text"
+                    value={hiddenCode}
+                    onChange={(e) => setHiddenCode(e.target.value)}
+                    style={{background: '#363636', color : '#C6C6C6', padding: '10px', borderRadius: '5px', border: '1px solid #A8A8A8', marginBottom: '20px', width: '400px'}}
+                    placeholder="히든코드를 입력해주세요."
+                />
+
+                <div style={{marginTop:'250px'}}>
+                    <button
+                        type="submit"
+                        style={{
+                            color: isButtonDisabled ? '#8D8D8D':'#0C0C0C',
+                            backgroundColor: isButtonDisabled ? '#525252' : '#1AE57C', // 버튼 비활성화 상태일 때와 활성화 상태일 때의 배경색 변경
+                            padding: '10px 30px',
+                            borderRadius: '15px',
+                            border: 'none',
+                            fontWeight: 'bold',
+                            width: '400px',
+                            cursor: isButtonDisabled ? 'not-allowed' : 'pointer', // 버튼 비활성화 상태일 때와 활성화 상태일 때의 커서 변경
+                            marginTop: 'auto' // 버튼을 하단으로 이동
+                        }}
+                        disabled={isButtonDisabled} // 버튼 활성화/비활성화
+                    >
+                        히든코드로 Geport 조회하기
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
---
+++ src/app/view/[id]/page.js
@@ -0,0 +1,71 @@
+"use client"
+import { useState, useEffect } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import Image from "next/image";
+import Link from "next/link";
+import {white} from "next/dist/lib/picocolors";
+
+export default function UserviewPage() {
+    const router = useRouter();
+    const params = useParams();
+    const id = params.id;
+    const username = "Can";
+    const intro_sentence = "잠은 죽어서 자..!";
+    // 페이지의 디자인 상태를 관리하는 상태 변수
+    const [showAlternateDesign, setShowAlternateDesign] = useState(false);
+
+    useEffect(() => {
+        // 3초 후에 다른 디자인을 보여주도록 타이머 설정
+        const timer = setTimeout(() => {
+            setShowAlternateDesign(true);
+        }, 3000);
+        // 컴포넌트가 언마운트되면 타이머를 정리하여 메모리 누수를 방지합니다.
+        return () => clearTimeout(timer);
+    }, []); // useEffect는 컴포넌트가 렌더링될 때 한 번만 실행됩니다.
+
+    return (
+        <div style={{ backgroundColor: "#181818", height: '100vh', color: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            {/* 조건부 렌더링을 사용하여 디자인을 변경 */}
+            {showAlternateDesign ? (
+                <div>
+                <div style={{
+                    backgroundColor: "#1AE57C",
+                    height: '50vh',
+                    width:'55vh',
+                    color: 'white',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'
+                }}>
+                    <div>
+                    <h2 style={{color:'black', fontSize: '24px'}}>{intro_sentence}</h2>
+                    <h2 style={{color:'black', fontSize: '24px'}}>{username}님의 iGeport</h2>
+                        <span style={{color: "gray"}}>{username}님의 블로그와 답변을 바탕으로</span>
+                        <br />
+                        <span style={{color: "gray"}}>퍼스널브랜딩에 필요한 심리 보고서를 준비했어요.</span>
+                    </div>
+                </div>
+                    <div style={ {display : 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                        <Link href="/">
+                            <button style={{color: '#0c0c0c', backgroundColor: '#1AE57C', padding: '10px 30px', borderRadius: '15px', width: '400px', fontWeight: 'bold', marginTop: '150px'}}>다음으로</button>
+                        </Link>
+                    </div>
+                </div>
+            ) : (
+                <div style={{ backgroundColor: "#181818", height: '100vh', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                    <div style={{ marginTop: '30px' }}>
+                        <Image src={"/image/logo.svg"} width={120} height={120} alt="Logo" />
+                    </div>
+                    <h2 style={{ color: 'white', fontSize: '24px', padding: '40px' }}>iGeport 결과 보고서</h2>
+                    <div style={{ marginTop: '30px', textAlign: 'center' }}>
+                        <span style={{ color: "#A8A8A8" }}>iGeport는 총 여섯 단계로</span>
+                        <br />
+                        <span style={{ color: "#A8A8A8" }}>블로그에 드러난 당신의 심리를 분석합니다.</span>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+    }
